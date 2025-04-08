9b81e6848d37223ace2ed87a19f25dc614d19838
caonguyenquoclam <99850767+QuocLammm@users.noreply.github.com>
2025-04-08 14:39:47 +0700

    update

--- myApp/config/menu.php
+++ myApp/config/menu.php
@@ -5,11 +5,7 @@ return [
         'route' => 'home',
         'icon' => 'ni ni-tv-2 text-primary',
     ],
-    [
-        'title' => 'Thanh Toán',
-        'route' => 'vnpay.payment.product',
-        'icon' => 'ni ni-tv-2 text-primary',
-    ],
+
     [
         'title' => 'Chức năng',
         'is_parent' => true,
@@ -31,25 +27,35 @@ return [
             ],
         ]
     ],
+    [
+        'title' => 'Tài khoản',
+        'is_parent' => true,
+        'children' => [
+            [
+                'title' => 'Nhân viên',
+                'route' => 'show-profile',
+                'icon' => 'ni ni-circle-08 text-dark',
+            ],
+            [
+                'title' => 'Khách hàng',
+                'route' => 'show-customer.index',
+                'icon' => 'ni ni-bullet-list-67 text-dark',
+            ],
+            [
+                'title' => 'Sản phẩm',
+                'route' => 'show-product.index',
+                'icon' => 'ni ni-basket text-dark',
+            ],
+            [
+                'title' => 'Thanh toán',
+                'route' => 'vnpay.payment.product',
+                'icon' => 'ni ni-credit-card text-primary',
+            ],
+        ]
+    ],
 //    [
-//        'title' => 'Tài khoản',
-//        'is_parent' => true,
-//        'children' => [
-//            [
-//                'title' => 'Nhân viên',
-//                'route' => 'show-profile',
-//                'icon' => 'ni ni-circle-08 text-dark',
-//            ],
-//            [
-//                'title' => 'Khách hàng',
-//                'route' => 'show-customer',
-//                'icon' => 'ni ni-bullet-list-67 text-dark',
-//            ],
-//            [
-//                'title' => 'Sản phẩm',
-//                'route' => 'show-product',
-//                'icon' => 'ni ni-basket text-dark',
-//            ],
-//        ]
+//        'title' => 'Thanh Toán',
+//        'route' => 'vnpay.payment.product',
+//        'icon' => 'ni ni-credit-card text-primary',
 //    ],
 ];
